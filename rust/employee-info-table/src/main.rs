244d748aaf67665eefb78aa0cb148a02608d6898
Dj-Viking <anders.swedishviking@gmail.com>
2025-08-24 00:45:44 -0400

    update packages, testing sql stuff in rust

--- rust/employee-info-table/src/main.rs
+++ rust/employee-info-table/src/main.rs
@@ -1,9 +1,20 @@
 use sqlite;
+
 fn main() {
-    let connection = sqlite::open(":memory:").unwrap();
-    let query = "select * from ed";
-    for row in connection.prepare(query).unwrap()
-    {
-        println!("yo we got rowssssss {:?}", row);
-    }
+  let connection = sqlite::open("test.db").unwrap();
+	println!("yooo");
+	// init_db(&connection);
+  // insert_dept(&connection);
+  read_dept(&connection);
+}
+
+fn read_dept(connection: &sqlite::Connection) {
+  let query = "select * from departments";
+  for row in connection.prepare(query).unwrap() {
+    println!("row {:?}", row);
+  }
+}
+fn insert_dept(connection: &sqlite::Connection) {
+  let query = "insert into departments (name) values ('hello')";
+  connection.prepare(query).unwrap();
 }
